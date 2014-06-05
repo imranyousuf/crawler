c38756387af8480aeffbc8d5b1107f00d81e2e90
Imran Yousuf <imranyousuf@berkleley.edu>
2014-06-05 16:38:04 -0700

    location does not work

--- im.js
+++ im.js
@@ -3,8 +3,8 @@ var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 var app     = express();
-urls =[];
-titiles=[];
+
+
 
 
 console.log('Got the return request, parsing now!!');
@@ -12,17 +12,52 @@ request('http://www.ticketmaster.com/', function (err,resp,body){
 	if(!err && resp.statusCode ==200){
 		
 		var $ = cheerio.load(body);
+		var allResults = [];
 
 		$('a.event','#onSaleList').each(function(){
 			//var url = $(this).attr('href')
 			//urls.push(url);
 			var title = $(this).text();
-			titiles.push(title);
+			var url = $(this).attr('href');
+
+			//var k = $('</a>').next();
+			//var loc = 
+		$('td.date').each(function(){
+			var month = $('div.month').text();
+			var day   = $('div.day').text();
+			var date  = $('div.date').text();
+
+/*
+		$('.event').each(function(i, element){
+
+			var l = $(this).parent().next();
+
+			var loc = l.text();
+		
+		/*
+			var location = $(this).text();
+			locations.push(location);
+	
+
+			*/
+		
+});
+
+		var metadata = {
+			//event_title: title,
+			//url: url
+			//location: loc
+			day: day
 
 
+		};
+
+		allResults.push(metadata);
+
+	//});
 		});
 
 		//console.log(urls);
-		console.log(titiles);
+		console.log(allResults);
 	}
 });
